7e5a54e4a63575e862a970fe98c9c878e139b16e
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2019-08-10 00:20:47 +0530

    basic working

---
+++ src/components/Info.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto my-6">
+    <div class="text-base sm:font-lg md:text-xl lg:text-xl xl:text-xl font-semibold">
+      A fullstack developer currently working in Javascript stack.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'info'
+}
+</script>
