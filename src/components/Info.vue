559fc4d85465facb05bc852efd9a2147bf4f31a7
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2019-08-13 22:19:31 +0530

    added short info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="container mx-auto my-6">
-    <div class="text-base sm:font-lg md:text-xl lg:text-xl xl:text-xl font-semibold">
-      A fullstack developer currently working in Javascript stack.
+    <div class="text-base px-3 sm:font-lg md:text-xl lg:text-xl xl:text-xl font-semibold text-justify">
+      <h3 class="text-xl">Skill set:</h3>
+      <ul class="list-disc sm:list-disc md:list-disc lg:list-disc xl:list-disc px-2">
+        <li class="text-gray-900 text-sm lg:text-lg md:text-lg xl:text-lg hover:text-gray-600">
+          <a href="" class="">Fullstack Developer</a>
+        </li>
+        <li class="text-gray-900 text-sm lg:text-lg md:text-lg xl:text-lg hover:text-gray-600">
+          <a href="">Polyglot Programmer</a>
+        </li>
+        <li class="text-gray-900 text-sm lg:text-lg md:text-lg xl:text-lg hover:text-gray-600">
+          <a href="">Hands on experience with multiple tech stacks</a>
+        </li>
+        <li class="text-gray-900 text-sm lg:text-lg md:text-lg xl:text-lg hover:text-gray-600">
+          <a href="">Graphic Designer</a>
+        </li>
+        <!-- <li class="text-sm lg:text-base md:text-base xl:text-base"></li> -->
+        <!-- <li class="text-sm lg:text-base md:text-base xl:text-base"></li> -->
+      </ul>
+      <!-- A fullstack developer currently working in Javascript stack, polyglot programmer in C, C++, Python, Golang and Javascript. Hands on experience 
+      in working with different tech stack like Node.js (Express, Nest, Koa, Hapi), Django (with Django Rest Framework), STL in C++, Golang (Gorilla, Beego, Revel, Gin)
+      in backend, Angular, React, Vue, Web Components (Stencil, Polymer, LitElement) with PWAs in web frontend and Android SDK, React Native, Nativescript and Flutter in
+      mobile frontend. -->
     </div>
   </div>
 </template>
